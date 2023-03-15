aba83eccf9126267fab78489e02cf59328a07e5a
Mohammed Ali Butt <abutt2001@hotmail.com>
2023-03-15 17:17:57 +0000

    .NET Sort implemented

---
+++ SortManager/SortManager/Model/DotNetSort.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace Model;
+
+public class DotNetSort : Sorter
+{
+    private Stopwatch stopWatch = Stopwatch.StartNew();
+
+    public override long GetMilliseconds()
+    {
+        return stopWatch.ElapsedMilliseconds;
+    }
+    public override long GetTicks()
+    {
+        return stopWatch.ElapsedTicks;
+    }
+
+    public override int[] Sort(int[] input)
+    {
+        stopWatch.Start();
+
+        Array.Sort(input);
+
+        stopWatch.Stop();
+
+        return input;
+    }
+}
