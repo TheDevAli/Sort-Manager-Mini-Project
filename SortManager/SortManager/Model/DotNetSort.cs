0dce0640e6ad390751b2e8a81722121028435c17
Mohammed Ali Butt <abutt2001@hotmail.com>
2023-03-15 16:42:17 +0000

    Added .NET Sort

---
+++ SortManager/SortManager/Model/DotNetSort.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace Model;
+
+public class DotNetSort : Sorter
+{
+    Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
+
+    private long _timelapse;
+    public override long Timelapse
+    {
+        get;
+    }
+
+    public override int[] Sort(int[] input)
+    {
+        stopWatch.Start();
+
+        Array.Sort(input);
+
+        stopWatch.Stop();
+
+        _timelapse = stopWatch.ElapsedMilliseconds;
+
+        return input;
+    }
+}
