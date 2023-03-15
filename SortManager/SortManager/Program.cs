3e689bc36846a35a71c1a76ad6418ac9fc841e5b
Alex Ho <cho@spartaglobal.com>
2023-03-15 12:31:44 +0000

    Added ArrayBuilder

---
+++ SortManager/SortManager/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SortManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ary = ArrayBuilder.GenArray(10);
+            
+            foreach(var item in ary)
+            {
+                Console.WriteLine(item) ;
+            }
+
+        }
+    }
+}
---
+++ SortManager/SortManager/View.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortManager
+{
+    public class View
+    {
+        public static int AskArraySize (int input)
+        {
+            Console.WriteLine();
+        }
+
+
+    }
+}
